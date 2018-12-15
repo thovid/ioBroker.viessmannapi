81fff3c733496fa3e2ce26d4b36e9073f12d34c4
Thomas Vidic <thomas.vidic@rewe-digital.com>
2018-12-15 21:00:15 +0100

    working on testing

---
+++ test/dummy.test.ts
@@ -0,0 +1,9 @@
+// tslint:disable:no-unused-expression
+import {expect} from 'chai';
+import 'mocha';
+
+describe('tests', () => {
+    it('should work', () => {
+        expect(true).to.be.true;
+    })
+});
